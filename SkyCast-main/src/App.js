87e9f43444aa5736c86fc6c60b67122258662c21
krishnappaL <lakshmihk2003@gmail.com>
2024-03-11 10:53:51 +0530

    My first React Project

---
+++ SkyCast-main/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import GfGWeatherApp from "./Components/SkyCast";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="w-11/12 lg:w-7/12 mx-auto">
+          <GfGWeatherApp/>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
